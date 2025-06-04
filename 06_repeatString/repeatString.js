35eaeb7863098438c3e138c6d40faa165afcf800
muthu-25 <68727855+muthu-25@users.noreply.github.com>
2025-06-04 06:59:54 +0000

    Created the necessary code to get the solution
    wrote the code required to satisfy all text cases successfully.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(name, count) {
+    if (count == 0){
+        return "";
+    } else if (count < 0){
+        return "ERROR";
+    }
+    let thestr = ""
+    for(let i = 0; i <count; i++ ){
+        thestr += name
+    }
+    return thestr;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
